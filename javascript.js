65c4d47dfbb644e41b3fea71b6b786fdc0cc4586
britoraf <rafaelbrito__@outlook.com>
2025-01-28 20:18:32 +0000

    'Rock, Paper, Scissors' - Buttons and winner result div added.

--- javascript.js
+++ javascript.js
@@ -1,60 +1,62 @@
 humanScore = 0
 computerScore = 0
 
-function playGame() {
-    for (let i = 0; i < 5; i++) {
-        playRound();
-        }
-        return "The final score is... HUMAN: " + humanScore + " CHEATING COMPUTER: " + computerScore + "."
-    }
 
-    function playRound(humanChoice, computerChoice) {
-        var humanChoice = getHumanChoice()
-        var computerChoice = getComputerChoice()
-    
-        function getComputerChoice() {
-            const rock = "rock"
-            const paper = "paper"
-            const scissors = "scissors"
-            const result = Math.ceil(Math.random() * 3)
-            if (result == 1) {
-                return rock;
-            } else if (result == 2) {
-                return paper;
-            } else {
-                return scissors;
-            }   
+const body = document.querySelector("body");
+
+// Buttons creation //
+const rockButton = document.createElement("button");
+rockButton.textContent = "Rock";
+const paperButton = document.createElement("button");
+paperButton.textContent = "Paper";
+const scissorsButton = document.createElement("button");
+scissorsButton.textContent = "Scissors";
+
+// Arranging the buttons inside body and before H3 //
+const h3 = document.querySelector("h3");
+body.insertBefore(rockButton, h3);
+body.insertBefore(paperButton, h3);
+body.insertBefore(scissorsButton, h3);
+
+// Buttons click event addition //
+rockButton.addEventListener("click", () => results.textContent = playRound("rock"));
+paperButton.addEventListener("click", () => results.textContent = playRound("paper"));
+scissorsButton.addEventListener("click", () => results.textContent = playRound("scissors"));
+
+// Div creation to show the game results //
+const results = document.createElement("div");
+body.insertBefore(results, h3);
+
+// Function playRound //
+function playRound(humanChoice, computerChoice) {
+  var computerChoice = getComputerChoice()
+
+  function getComputerChoice() {
+      const rock = "rock"
+      const paper = "paper"
+      const scissors = "scissors"
+      const result = Math.ceil(Math.random() * 3)
+      if (result == 1) {
+          return rock;
+      } else if (result == 2) {
+          return paper;
+      } else {
+          return scissors;
+      }
+  }
+    if (humanChoice == "paper" && computerChoice == "rock" ||
+        humanChoice == "scissors" && computerChoice == "paper" ||
+        humanChoice == "rock" && computerChoice == "scissors") {
+            humanScore++;
+            return `HE HE BOY You win! ${humanChoice} destroys ${computerChoice}.`
         }
-        function getHumanChoice(choice) {
-            const rock = "rock"
-            const paper = "paper"
-            const scissors = "scissors"
-            var choice = prompt("Choose one: Rock, Paper or Scissors?").toLowerCase();
-            if (choice == "rock") {
-                return rock;
-            }
-            else if (choice == "paper") {
-                return paper;
-            }
-            else if (choice == "scissors") {
-                return scissors;
-            } else {
-                console.log("You have to choose, my boy.")
-            }
+    else if (humanChoice == "rock" && computerChoice == "paper" ||
+            humanChoice == "paper" && computerChoice == "scissors" ||
+            humanChoice == "scissors" && computerChoice == "rock") {
+            computerScore++;
+            return `You lose, dude! Damn this game. ${computerChoice} destroys ${humanChoice}.`
         }
-        if (humanChoice == "paper" && computerChoice == "rock" ||
-            humanChoice == "scissors" && computerChoice == "paper" ||
-            humanChoice == "rock" && computerChoice == "scissors") {
-                humanScore++;
-                console.log(`HE HE BOY You win! ${humanChoice} destroys ${computerChoice}.`)
-            }
-        else if (humanChoice == "rock" && computerChoice == "paper" ||
-                humanChoice == "paper" && computerChoice == "scissors" ||
-                humanChoice == "scissors" && computerChoice == "rock") {
-                computerScore++;
-                console.log(`You lose, dude! Damn this game. ${computerChoice} destroys ${humanChoice}.`)
-            }
-        else {
-                console.log(`Tie.`)
-        }
+    else {
+            return `Tie.`
     }
+}
